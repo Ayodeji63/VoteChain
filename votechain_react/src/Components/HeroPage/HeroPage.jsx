f53edc8280dda1125884808652ff60c45d21dc60
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-22 16:57:03 +0100

    Started Working on the Login Component

--- votechain_react/src/Components/HeroPage/HeroPage.jsx
+++ votechain_react/src/Components/HeroPage/HeroPage.jsx
@@ -4,6 +4,13 @@ import "./HeroPage.css"
 const HeroPage = () => {
   return (
     <div className='heropage-container'>
+        <div className='heropage-texts'>
+            <h3>
+                Vote For <span>Change,</span><br/>
+                Vote <span>Wisely</span>
+            </h3>
+            <p>Powerful, self-serve product and growth<br/> analytics to help you convert, engage, and <br/>retain more users. Trusted by over 4,000 <br/>startups.</p>
+        </div>
 
     </div>
   )
