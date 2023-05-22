3caea4b2c4c55b2985d8d84a4c4b02b341b961b9
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-22 09:58:57 +0100

    Started Working on the Navbar Component

--- votechain_react/src/Components/NavBar/Navbar.jsx
+++ votechain_react/src/Components/NavBar/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./Navbar.css";
-import {voteLogo} from "../../images/demo-logo.png"
+import voteLogo from "../../images/demo-logo.png";
 
 const Navbar = () => {
   return (
     <header>
       <div className="navbar-container">
-        <img src={voteLogo} alt="Vote Logo"/>
+        <img src={voteLogo} alt="Vote Logo" />
+        <ul>
+          <li>
+            <a href="/">Mission</a>
+          </li>
+          <li>
+            <a href="/">How To Vote</a>
+          </li>
+          <li>
+            <a href="/">FAQs</a>
+          </li>
+        </ul>
+        <button>Get Started</button>
       </div>
     </header>
   );
