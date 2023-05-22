f18eade970f1baf8708d672f8b3e28bcd025ecfe
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-22 10:47:24 +0100

    Started Working on the Navbar Component

--- votechain_react/src/App.js
+++ votechain_react/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import Navbar from './Components/NavBar/Navbar';
+import "./App.css";
+import Navbar from "./Components/NavBar/Navbar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Navbar />} />
+        </Routes>
+      </Router>
     </>
   );
 }
