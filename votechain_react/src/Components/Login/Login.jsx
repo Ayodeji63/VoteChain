95df0541d47a842edd9cd0aa153bedeed0cbe3f7
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-22 11:18:00 +0100

    Started Working on the Login Component

--- votechain_react/src/Components/Login/Login.jsx
+++ votechain_react/src/Components/Login/Login.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import "./Login.css"
+import React from "react";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div className='login-container'>
-        <div className='login-form'>
-            <h3>Log in to your account </h3>
-            <p>Enter your Voters Identification Number (VIN) or National <br/>Identification Number(NIN)</p>
-            <input type="text" placeholder="Enter your VIN/NIN"/>
-            <input type="password" placeholder="Enter your Password"/>
-            <button>Login</button>
-            <p>Don't have an account? </p><a href="/register">Register</a>
-        </div>
+    <div className="login-container">
+      <div className="login-form">
+        <h3>Log in to your account </h3>
+        <p>
+          Enter your Voters Identification Number (VIN) or National <br />
+          Identification Number(NIN)
+        </p>
+        <input type="text" placeholder="Enter your VIN/NIN" />
+        <input type="password" placeholder="Enter your Password" />
+        <button>Login</button>
+        <span>Don't have an account? </span>
+        <a href="/register">Register</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
