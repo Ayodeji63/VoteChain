fa46335166b0b77f6a0a63c5bc8fa782fe490f52
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-22 17:42:32 +0100

    Started Working on the SubHeroPage Component

--- votechain_react/src/Components/SubHeroPage/SubHeroPage.jsx
+++ votechain_react/src/Components/SubHeroPage/SubHeroPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./SubHeroPage.css";
+import firstSubHeroPage from "../../images/sub-hero-image1.png";
+import secondSubHeroPage from "../../images/sub-hero-image2.png";
 
 const SubHeroPage = () => {
   return (
     <div className="super-hero-container">
       <div className="super-hero-images">
-        <div></div>
-        <div></div>
+        <div>
+          <img src={firstSubHeroPage} alt="" />
+        </div>
+        <div>
+          <img src={secondSubHeroPage} alt="" />
+        </div>
       </div>
     </div>
   );
