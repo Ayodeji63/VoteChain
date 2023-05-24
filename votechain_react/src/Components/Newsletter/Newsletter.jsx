7203d69c6bb30848f439fb031915b09dcb0bd33e
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-24 13:47:29 +0100

    Started working on the Newsletter

--- votechain_react/src/Components/Newsletter/Newsletter.jsx
+++ votechain_react/src/Components/Newsletter/Newsletter.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Newsletter.css";
+import newsLetterImage1 from "../../images/newletter-image-1.png";
+import newsLetterImage2 from "../../images/newletter-image-2.png";
+import newsLetterImage3 from "../../images/newletter-image-3.png";
 
 const Newsletter = () => {
   return (
@@ -7,6 +10,11 @@ const Newsletter = () => {
       <div className="newsletter-text">
         <h4>Newsletter</h4>
         <p>Everything you need to know about the product and billing.</p>
+      </div>
+      <div className="newsletter-cards">
+        <div className="newsletter-card">
+          <img src="" alt="" />
+        </div>
       </div>
     </div>
   );
