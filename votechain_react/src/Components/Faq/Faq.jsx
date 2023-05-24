5c67e3ab32c029667274b7c18057dda42b2277e8
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-24 12:57:50 +0100

    Started working on the Faq Component

--- votechain_react/src/Components/Faq/Faq.jsx
+++ votechain_react/src/Components/Faq/Faq.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Faq.css";
 
 const Faq = () => {
-  return <div>Faq</div>;
+  return (
+    <div className="faq-container">
+      <div className="faq-text">
+        <h4>Frequently asked questions</h4>
+        <p>Everything you need to know about the product and billing.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
