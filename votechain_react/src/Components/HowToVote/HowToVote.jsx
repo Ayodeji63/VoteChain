3a7f7c3fa26a683442de1f0b9617adb95ee63643
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-24 11:45:22 +0100

    Started working on the How to vote Component

--- votechain_react/src/Components/HowToVote/HowToVote.jsx
+++ votechain_react/src/Components/HowToVote/HowToVote.jsx
@@ -1,11 +1,44 @@
 import React from "react";
 import "./HowtoVote.css";
+import inboxIcon from "../../images/inbox icon.png";
+import instantIcon from "../../images/instant icon.png";
+import reportIcon from "../../images/report icon.png";
 
 const HowToVote = () => {
   return (
     <div className="how-to-vote-container">
       <div className="how-to-vote-details">
-        <div className="how-to-vote-cards"></div>
+        <h4>How to Vote</h4>
+        <div className="how-to-vote-cards">
+          <div className="how-to-vote-card">
+            <img src={inboxIcon} alt="Inbox Icon" />
+            <h5>Share team inboxes</h5>
+            <p>
+              Whether you have a team of 2 or 200, our shared <br />
+              team inboxes keep everyone on the same page
+              <br /> and in the loop.
+            </p>
+          </div>
+          <div className="how-to-vote-card">
+            <img src={instantIcon} alt="Inbox Icon" />
+            <h5>Deliver instant answers</h5>
+            <p>
+              An all-in-one customer service platform that
+              <br /> helps you balance everything your customers <br />
+              need to be happy.
+            </p>
+          </div>
+
+          <div className="how-to-vote-card">
+            <img src={reportIcon} alt="Inbox Icon" />
+            <h5>Manage your team with reports</h5>
+            <p>
+              Measure what matters with Untitled’s easy-to-use <br />
+              reports. You can filter, export, and drilldown on
+              <br /> the data in a couple clicks.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
