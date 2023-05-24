eb8715db14c00f98ee48857a1d9f6649564a0034
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-24 14:11:18 +0100

    Started working on the Newsletter

--- votechain_react/src/Components/Newsletter/Newsletter.jsx
+++ votechain_react/src/Components/Newsletter/Newsletter.jsx
@@ -13,7 +13,39 @@ const Newsletter = () => {
       </div>
       <div className="newsletter-cards">
         <div className="newsletter-card">
-          <img src="" alt="" />
+          <img src={newsLetterImage1} alt="News Letter Illustration" />
+          <p className="newsletter-date">Natali Craig • 14 Jan 2022</p>
+          <a href="##">
+            Nigeria election results: Peter Obi lands
+            <br /> surprise win in Lagos State
+          </a>
+          <p className="newsletter-brief">
+            Collaboration can make our teams stronger, and our <br />
+            individual designs better.
+          </p>
+        </div>
+        <div className="newsletter-card">
+          <img src={newsLetterImage2} alt="News Letter Illustration" />
+          <p className="newsletter-date">Drew Cano • 13 Jan 2022</p>
+          <a href="##">
+            Nigeria's Elections Risk Sowing <br />
+            Cynicism, Mistrust | Council on Foreign Relations
+          </a>
+          <p className="newsletter-brief">
+            JavaScript frameworks make development easy with <br />
+            extensive features and functionalities.
+          </p>
+        </div>
+        <div className="newsletter-card">
+          <img src={newsLetterImage3} alt="News Letter Illustration" />
+          <p className="newsletter-date">Orlando Diggs • 12 Jan 2022</p>
+          <a href="##">
+            Nigeria presidential election 2023 |<br /> Financial Times
+          </a>
+          <p className="newsletter-brief">
+            Starting a community doesn’t need to be
+            <br /> complicated, but how do you get started?
+          </p>
         </div>
       </div>
     </div>
