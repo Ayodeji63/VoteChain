41c1fe39370bfd25816a4bc720d7863e14d9860e
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-25 11:34:39 +0100

    Started working on the LiveResults Component

--- votechain_react/src/Components/LiveResults/LiveResults.jsx
+++ votechain_react/src/Components/LiveResults/LiveResults.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./LiveResults.css";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 const LiveResults = () => {
-  return <div>LiveResults</div>;
+  return (
+    <div className="live-result-container">
+      <div className="live-result-details">
+        <h5>Live Results</h5>
+        <div className="live-results-cards">
+          <div className="live-results-card">
+            <h5>Presidential elections</h5>
+            <span>Counted Votes</span>
+            <h5>12,420,567</h5>
+            <p>
+              <span>
+                <AiOutlineArrowUp /> 60%{" "}
+              </span>
+              Labour Party
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LiveResults;
