a7035eba86ee34f1f0a784657cdfc1972b1dfd51
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-27 11:36:49 +0100

    Started working on the Final Results Component

--- votechain_react/src/Components/FinalResults/FinalResults.jsx
+++ votechain_react/src/Components/FinalResults/FinalResults.jsx
@@ -1,9 +1,52 @@
 import React from "react";
-import { Table } from "antd";
+import { Table, Tag } from "antd";
+import firstCandidates from "../../images/candidates.png";
 import { AiOutlineSearch } from "react-icons/ai";
 import "./FinalResults.css";
 
 const FinalResults = () => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a href="##">{text}</a>
+    },
+    {
+      title: "PARTY",
+      dataIndex: "party",
+      key: "party",
+    },
+    {
+      title: "TOTAL VOTES",
+      dataIndex: "total votes",
+      key: "address",
+    },
+    {
+      title: "ACTION",
+      key: "action",
+      render: () => (
+        <Tag color="green">Vote</Tag>
+      ),
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      name:
+        <img src={firstCandidates} alt="firs-candidate" /> +
+        "Imumolen Christopher",
+      party: "Accord Party (AP)",
+      totalVotes: 12,
+    },
+    {
+      key: "2",
+      name: "Imumolen Christopher",
+      party: "Accord Party (AP)",
+      totalVotes: 12,
+    },
+  ];
   return (
     <div className="final-results-container">
       <div className="final-results-text-search">
@@ -18,6 +61,8 @@ const FinalResults = () => {
         </div>
       </div>
       <p className="aspirants">21 Aspirants</p>
+
+      <Table columns={columns} dataSource={data} />
     </div>
   );
 };
