8bb0cf3e1230e644b311172eb9f039b052d5b85c
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-28 16:57:44 +0100

    Final Results Table done

--- votechain_react/src/Components/FinalResults/data.js
+++ votechain_react/src/Components/FinalResults/data.js
@@ -1,4 +1,6 @@
 import firstCandidates from "../../images/candidates.png";
+import secondCandidate from "../../images/secondCandidate.png";
+import thirdCandidate from "../../images/thirdcandidate.png";
 export const data = [
   {
     key: "1",
@@ -12,11 +14,13 @@ export const data = [
     name: "Al-Mustapha Hamza",
     party: "Action Alliance (AA)",
     totalVotes: 15,
+    candidateImage: secondCandidate,
   },
   {
     key: "3",
-    name: "Al-Mustapha Hamza",
-    party: "Action Alliance (AA)",
-    totalVotes: 15,
+    name: "Sowore Omoyele",
+    party: "African Action Congress(AAC)",
+    totalVotes: "145,000",
+    candidateImage: thirdCandidate,
   },
 ];
