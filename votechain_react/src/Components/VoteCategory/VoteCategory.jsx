b410e499f1ad6d315857cd0db4d3b83db62bea94
Olagboye Seyi <akingbolaola@gmail.com>
2023-05-29 16:44:46 +0100

    Updated some code base

--- votechain_react/src/Components/VoteCategory/VoteCategory.jsx
+++ votechain_react/src/Components/VoteCategory/VoteCategory.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { AiOutlineRight } from "react-icons/ai";
 import "./VoteCategory.css";
 
 const VoteCategory = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/finalresults");
+  };
   return (
     <div className="vote-category-container">
       <div className="vote-details">
         <h5>Vote</h5>
         <div className="vote-category-cards">
-          <div className="vote-category-card">
+          <div className="vote-category-card" onClick={handleNavigate}>
             <h5>
               Presidential elections <AiOutlineRight className="vote-icon" />
             </h5>
