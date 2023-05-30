ec2c04f364f9bbaeefd5621732bbe3be0420cb06
Ayodeji <olusanyasamson633@gmail.com>
2023-05-30 20:39:10 +0100

    integrated_blocto_login

--- votechain_react/src/Components/Login/Login.jsx
+++ votechain_react/src/Components/Login/Login.jsx
@@ -1,29 +1,34 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+import { Web3Button } from "@web3modal/react"
+import App from "../blocto_test/App"
 
 const Login = () => {
-  const navigate = useNavigate();
-  const handleNavigate = () => {
-    navigate("/welcome");
-  };
-  return (
-    <div className="login-container">
-      <div className="login-form">
-        <h3>Log in to your account </h3>
-        <p>
-          Enter your Voters Identification Number (VIN) or National <br />
-          Identification Number(NIN)
-        </p>
-        <input type="text" placeholder="Enter your VIN/NIN" />
-        <input type="email" placeholder="Enter your Email Address" />
-        <button onClick={handleNavigate}>Login</button>
-        <span>
-          Don't have an account? <a href="/register">Register</a>
-        </span>
-      </div>
-    </div>
-  );
-};
+    const navigate = useNavigate()
+    const handleNavigate = () => {
+        navigate("/welcome")
+    }
+    return (
+        // <div className="login-container">
+        //   <div className="login-form">
+        //     <h3>Log in to your account </h3>
+        //     <p>
+        //       Enter your Voters Identification Number (VIN) or National <br />
+        //       Identification Number(NIN)
+        //     </p>
+        //     <input type="text" placeholder="Enter your VIN/NIN" />
+        //     <input type="email" placeholder="Enter your Email Address" />
+        //     <button onClick={handleNavigate}>Login</button>
+        //     <span>
+        //       Don't have an account? <a href="/register">Register</a>
+        //     </span>
+        //   </div>
+        // </div>
+        <>
+            <App />
+        </>
+    )
+}
 
-export default Login;
+export default Login
