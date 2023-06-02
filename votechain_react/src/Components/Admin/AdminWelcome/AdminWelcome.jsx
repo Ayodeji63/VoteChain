bf09cbebb805a9d92084fb526603c02f1eb873fe
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-02 10:47:31 +0100

    Started working on the Admin dashboard

--- votechain_react/src/Components/Admin/AdminWelcome/AdminWelcome.jsx
+++ votechain_react/src/Components/Admin/AdminWelcome/AdminWelcome.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const AdminWelcome = () => {
   return (
-    <div>AdminWelcome</div>
-  )
-}
+    <div className="admin-welcome-container">
+      <div className="admin-welcome-text-button">
+        <div className="admin-welcome-text">
+          <h3>Welcome back, Admin</h3>
+        </div>
+        <div className="admin-welcome-button">
+          <button>Create New Poll</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AdminWelcome
+export default AdminWelcome;
