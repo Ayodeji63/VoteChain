1ab3e649471f764b733e5f1ea52ac053f4ec32c4
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-02 11:14:46 +0100

    Started working on the Recent Polls Component

--- votechain_react/src/Components/Admin/RecentPolls/RecentPolls.jsx
+++ votechain_react/src/Components/Admin/RecentPolls/RecentPolls.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { Table } from "antd";
 import "./RecentPolls.css";
 
 const RecentPolls = () => {
-  return <div>RecentPolls</div>;
+  const columns = [
+    {
+      title: "NAME OF POLL",
+      dataIndex: "nameofpoll",
+      key: "nameofpoll",
+    },
+    {
+      title: "CANDIDATES",
+      dataIndex: "candidates",
+      key: "candidates",
+    },
+    {
+      title: "STATUS",
+      dataIndex: "status",
+      key: "status",
+    },
+  ];
+  return (
+    <div className="recent-polls-container">
+      <div className="recent-polls-table"></div>
+    </div>
+  );
 };
 
 export default RecentPolls;
