cace34cd03098856f79db0bdde47cfc3c960c751
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-03 11:27:36 +0100

    Started Create-new-Poll Modal

--- votechain_react/src/Components/Admin/CreatePoll/CreatePoll.jsx
+++ votechain_react/src/Components/Admin/CreatePoll/CreatePoll.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreatePoll.css";
+import { Modal } from "antd";
 import { AiOutlinePlus } from "react-icons/ai";
 import candidateAvatarUpload from "../../../images/candidate-avatar-upload.png";
 
 const CreatePoll = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="create-poll-container">
       <h4>Create New Poll</h4>
@@ -22,10 +34,16 @@ const CreatePoll = () => {
           <h5>Candice WU</h5>
           <p>APC</p>
         </div>
-        <div className="add-new-candidate">
+        <div className="add-new-candidate" onClick={showModal}>
           <AiOutlinePlus className="add-icon" />
           <p>Add new candidate</p>
         </div>
+        <Modal open={isModalOpen} onCancel={handleCancel} footer={null}>
+          <div>
+            <h4>Enter candidate details</h4>
+          </div>
+        </Modal>
+
         <div className="create-buttons">
           <button className="cancel-btn">Cancel</button>
           <button className="create-btn">Create poll</button>
