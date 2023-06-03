48fe37cd867db08c983e93edf0caf23076f6fdd8
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-03 12:01:47 +0100

    Started Create-new-Poll Modal

--- votechain_react/src/Components/Admin/CreatePoll/CreatePoll.jsx
+++ votechain_react/src/Components/Admin/CreatePoll/CreatePoll.jsx
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
 import "./CreatePoll.css";
 import { AiOutlineInbox } from "react-icons/ai";
-import { Modal, Upload, Message } from "antd";
+import { Modal, Upload, message } from "antd";
 import { AiOutlinePlus } from "react-icons/ai";
 import candidateAvatarUpload from "../../../images/candidate-avatar-upload.png";
 
 const CreatePoll = () => {
+  const { Dragger } = Upload;
+  const props = {
+    name: "file",
+    multiple: true,
+    action: "https://",
+    onchange(info) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        message.error(`${info.file.name} file uploaded failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const showModal = () => {
