772a62112dda93f0cd30a9e022bf6dbcf31fd51f
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-08 17:05:44 +0100

    Started working on the SubHeroPage Component

--- votechain__next/src/components/Mission/Mission.jsx
+++ votechain__next/src/components/Mission/Mission.jsx
@@ -1,40 +1,37 @@
-import React from "react"
-import "./Mission.css"
-import Image from "next/image"
+import React from "react";
+import "./Mission.css";
+import Image from "next/image";
 
 const Mission = () => {
-    return (
-        <div className="mission-container" id="mission">
-            <div className="mission-details">
-                <div className="mission-image">
-                    <img
-                        src={"/images/mission-image.png"}
-                        alt="Mission Illustration"
-                    />
-                </div>
-                <div className="mission-text">
-                    <h5>Our mission</h5>
-                    <p>
-                        Whether you have a team of 2 or 200, our shared team{" "}
-                        <br /> inboxes keep everyone on the same page and in the
-                        loop.
-                    </p>
-                    <div className="highlight-missions">
-                        <img src={"/images/Check icon.png"} alt="Check Icon" />
-                        <p>Leverage automation to move fast</p>
-                    </div>
-                    <div className="highlight-missions">
-                        <img src={"/images/Check icon.png"} alt="Check Icon" />
-                        <p>Always give customers a human to chat to</p>
-                    </div>
-                    <div className="highlight-missions">
-                        <img src={"/images/Check icon.png"} alt="Check Icon" />
-                        <p>Always give customers a human to chat to</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="mission-container" id="mission">
+      <div className="mission-details">
+        <div className="mission-image">
+          <img src={"/images/mission-image.png"} alt="Mission Illustration" />
         </div>
-    )
-}
+        <div className="mission-text">
+          <h5>Our mission</h5>
+          <p>
+            The goal and the plan of the team is to show the power of blockchain
+            and decentralization using some tools like the ChainLink Automation
+            we have been able to :
+          </p>
+          <div className="highlight-missions">
+            <img src={"/images/Check icon.png"} alt="Check Icon" />
+            <p>Automate Announcement of Winner base on Results</p>
+          </div>
+          <div className="highlight-missions">
+            <img src={"/images/Check icon.png"} alt="Check Icon" />
+            <p>Count the Amount of Votes Voted for each Candidate</p>
+          </div>
+          <div className="highlight-missions">
+            <img src={"/images/Check icon.png"} alt="Check Icon" />
+            <p>Allow users and electorate have a free and fair voting process</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Mission
+export default Mission;
