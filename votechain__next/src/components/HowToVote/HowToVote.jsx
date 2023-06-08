9fa06ed8a69edc6ff92b215791b37d69ae479205
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-08 17:50:18 +0100

    Finished most of the text on the website

--- votechain__next/src/components/HowToVote/HowToVote.jsx
+++ votechain__next/src/components/HowToVote/HowToVote.jsx
@@ -1,53 +1,48 @@
 import React from "react";
 import "./HowtoVote.css";
+import { AiOutlineWallet } from "react-icons/ai";
 import Image from "next/image";
 const HowToVote = () => {
   return (
     <div className="how-to-vote-container" id="howtovote">
       <div className="how-to-vote-details">
         <h4>How to Vote</h4>
+        <p>
+          We have built Votechain to be as simple to use as possible,
+          <br /> here are the steps to be able to vote for your favorite
+          candidate
+        </p>
         <div className="how-to-vote-cards">
           <div className="how-to-vote-card">
-            <Image
-              src={"/images/inbox icon.png"}
-              width={100}
-              height={100}
-              alt="Inbox Icon"
-            />
-            <h5>Share team inboxes</h5>
+            <h5>Connect Wallet</h5>
             <p>
-              Whether you have a team of 2 or 200, our shared <br />
-              team inboxes keep everyone on the same page
-              <br /> and in the loop.
+              The first required step to be able <br />
+              to vote is to connect your decentralization
+              <br /> wallet e.g(MetaMask, Trust Wallet etc)
             </p>
           </div>
           <div className="how-to-vote-card">
-            <Image
+            {/* <Image
               src={"/images/instant icon.png"}
               alt="Inbox Icon"
               width={100}
               height={100}
-            />
-            <h5>Deliver instant answers</h5>
+            /> */}
+            <h5>Register to Vote</h5>
             <p>
-              An all-in-one customer service platform that
-              <br /> helps you balance everything your customers <br />
-              need to be happy.
+              The next step is to register
+              <br /> to be able to vote and you are required to provide <br />
+              your National Identity Number.
             </p>
           </div>
 
           <div className="how-to-vote-card">
-            <Image
-              src={"/images/report icon.png"}
-              alt="Inbox Icon"
-              width={100}
-              height={100}
-            />
-            <h5>Manage your team with reports</h5>
+            <h5>
+              Pick your Favorite <br />
+              Candidate and Vote
+            </h5>
             <p>
-              Measure what matters with Untitled’s easy-to-use <br />
-              reports. You can filter, export, and drilldown on
-              <br /> the data in a couple clicks.
+              After successful Registeration,<br/> go ahead and navigate to the voting page <br/>and cast your vote and await <br/>the announcement of winner
             </p>
           </div>
         </div>
