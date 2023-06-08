86fd95106151e521462911d337e58d4a35910795
Olagboye Seyi <akingbolaola@gmail.com>
2023-06-08 11:48:42 +0100

    Sorted out the smooth scoll

--- votechain__next/src/components/Navbar/Navbar.jsx
+++ votechain__next/src/components/Navbar/Navbar.jsx
@@ -1,45 +1,45 @@
-import React, { useContext, useEffect } from "react"
-import "./Navbar.css"
-import { SignIn } from "../ConnectButton/ConnectButton"
-import Link from "next/link"
-import Image from "next/image"
-import { useRouter } from "next/navigation"
-import { useAccount } from "wagmi"
+import React, { useContext, useEffect } from "react";
+import "./Navbar.css";
+import { SignIn } from "../ConnectButton/ConnectButton";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useAccount } from "wagmi";
 
 const Navbar = () => {
-    // const navigate = useNavigate()
-    const { address } = useAccount()
-    const router = useRouter()
-    useEffect(() => {
-        address ? router.push("/welcome") : router.push("/")
-    }, [address])
-    return (
-        <header>
-            <div className="navbar-container">
-                <Link href={"/"}>
-                    <Image
-                        src={"/VoteChainLogo.png"}
-                        width={100}
-                        height={100}
-                        alt="Vote Logo"
-                    />
-                </Link>
-                <ul className="nav-list">
-                    <li>
-                        <a href="/#mission">Mission</a>
-                    </li>
-                    <li>
-                        <a href="/#howtovote">How To Vote</a>
-                    </li>
-                    <li>
-                        <a href="/#faqs">FAQs</a>
-                    </li>
-                </ul>
+  // const navigate = useNavigate()
+  const { address } = useAccount();
+  const router = useRouter();
+  useEffect(() => {
+    address ? router.push("/welcome") : router.push("/");
+  }, [address]);
+  return (
+    <header>
+      <div className="navbar-container">
+        <Link href={"/"}>
+          <Image
+            src={"/VoteChainLogo.png"}
+            width={130}
+            height={100}
+            alt="Vote Logo"
+          />
+        </Link>
+        <ul className="nav-list">
+          <li>
+            <a href="/#mission">Mission</a>
+          </li>
+          <li>
+            <a href="/#howtovote">How To Vote</a>
+          </li>
+          <li>
+            <a href="/#faqs">FAQs</a>
+          </li>
+        </ul>
 
-                <SignIn showBalance={false} className="hero-page-btn" />
-            </div>
-        </header>
-    )
-}
+        <SignIn showBalance={false} className="hero-page-btn" />
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
